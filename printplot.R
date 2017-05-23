a0b793d882c61e5dacf2d525ff19bf7a2398cd0b
alessanf <alessandro.lin@hotmail.com>
2017-05-22 20:27:28 -0700

    dendograms

--- printplot.R
+++ printplot.R
@@ -1,3 +1,5 @@
 data <- read.csv("C:/Users/aless/Documents/GitHub/CS434/finalres.csv")
-
+data2 <- read.csv("C:/Users/aless/Documents/GitHub/CS434/result.csv")
 plot(data, ylab="SSE", xlab="k")
+
+plot(data2, ylab="SSE", main="SSE for k = 2")
