d21e65dfd6f7e4e6a380bc86a6c2e9b3865a3f1c
MrFengShui <luansongjian@gmail.com>
2017-05-22 17:45:00 -0700

    add result.txt file

--- assignment4test.py
+++ assignment4test.py
@@ -199,5 +199,5 @@ if __name__ == "__main__":
 	# for i in range(2, 11):
 	# 	kmeans(data, i, 1000000)
 	data =  read_from_file()
-	# hac_single_link(data)
+	hac_single_link(data)
 	hac_complete_link(data)
